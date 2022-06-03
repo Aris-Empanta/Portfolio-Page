f78c501182bc47444ba20e8c5771fe672c060cf5
Aris Empanta <eams220891@gmail.com>
2022-06-03 18:27:19 +0300

    Some changes at home section

---
+++ js/4_projectSection.js
@@ -0,0 +1,38 @@
+/*With the below function, when i scroll down to the project section, the title 'My work' appears gradually. */
+document.addEventListener('scroll', () => {
+    function showTitle() {
+        const title = document.getElementById('projectTitle')
+        //the number of pixels the element is scrolled down
+        const currentScrollPosition = window.pageYOffset;
+        //The distance of the top border of the element with id=projectsSection, to the top of the parent element which is 'body' 
+        const elementOffsetTop = document.getElementById('projectsSection').offsetTop
+        /*The condition below, will have as a result, the projects to appear slowly only if scroll anywhere inside the 
+        project section and a little bit above*/
+        if(currentScrollPosition + 500 > elementOffsetTop &&
+            currentScrollPosition <= elementOffsetTop + 1250 ){            
+            title.style.opacity = 1
+          }
+        }   
+    setTimeout(showTitle, 500)
+});
+
+
+/*With the below function, when i scroll down to the project section, the projects appear gradually. */
+document.addEventListener('scroll', () => {
+    function showProjects() {
+        const projects = document.getElementsByClassName('myProjects')
+        //the number of pixels the element is scrolled down
+        const currentScrollPosition = window.pageYOffset;
+        //The distance of the top border of the element with id=projectsSection, to the top of the parent element which is 'body' 
+        const elementOffsetTop = document.getElementById('projectsSection').offsetTop
+        /*The condition below, will have as a result, the projects to appear slowly only if scroll anywhere inside the 
+        project section and a little bit above*/
+        if(currentScrollPosition + 500 > elementOffsetTop &&
+            currentScrollPosition <= elementOffsetTop + 1250 ){            
+            for (i = 0; i < projects.length; i++) {
+            projects[i].style.opacity = 1
+          }
+        }       
+    }
+    setTimeout(showProjects, 1500)
+});
