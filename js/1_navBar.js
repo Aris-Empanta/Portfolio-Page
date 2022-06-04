6f2b28140b770b2c827295862355d2abe393c619
Aris Empanta <eams220891@gmail.com>
2022-06-05 00:28:05 +0300

    Some changes on navbar

--- js/1_navBar.js
+++ js/1_navBar.js
@@ -2,7 +2,7 @@ let menu = document.getElementById("navWrapper");
 let about = document.getElementById('about') 
 let projects = document.getElementById('projects')
 let contact = document.getElementById('contact')
-
+let github = document.getElementById('github')
 
 
 /*With below function, once we press the bars icon in smaller screens,
@@ -13,6 +13,7 @@ const getResponsive = () => {
       about.style.fontSize = 0
       projects.style.fontSize = 0
       contact.style.fontSize = 0 
+      github.style.fontSize = 0 
       menu.className += " showMenu";
         setTimeout(() => {
           about.style.fontSize = '1.5rem'
@@ -23,7 +24,16 @@ const getResponsive = () => {
         setTimeout(() => {
           contact.style.fontSize = '1.5rem'
         },1250)
+        setTimeout(() => {
+          github.style.fontSize = '1.5rem'
+        },1750)
       }else if (menu.className === "navBar showMenu" )  {
         menu.className = "navBar"
       } 
+}
+
+/*With below functions, once we press a link on navbar in smaller screens,
+the menu collapses and remains only the bars icon. */
+const hideMenu = () => {
+  menu.className = "navBar"
 }
