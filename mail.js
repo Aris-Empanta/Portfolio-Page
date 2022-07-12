b99e0c3efceddbc667906dda577fc50292d4a91b
Aris Empanta <eams220891@gmail.com>
2022-07-13 00:31:58 +0300

    Finishing portfolio page

---
+++ mail.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const app = express()
+const nodemailer = require('nodemailer');
+const bodyParser = require("body-parser");
+const path = require('path')
+const cors = require("cors")
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(cors())
+app.set("view engine", "ejs")
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html")
+})
+
+
+
+
+
+app.post("/", (req, res) => {
+
+  let transporter = nodemailer.createTransport({
+    service: 'outlook',
+    auth: {
+      user: 'admiral_akainu@outlook.com',
+      pass: 'marinehq1991'
+    }
+  });
+  
+  let mailOptions = {
+    from: 'admiral_akainu@outlook.com',
+    to: 'eams220891@gmail.com',
+    subject: req.body.subject,
+    text: `hey bro ${req.body.name}`
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(`<script>alert("Email Sent Successfully.")</script>`);
+    }
+  })
+  res.redirect("/#contactSection")
+});
+
+app.listen(5000, () => console.log("Listening to port 5000"))
